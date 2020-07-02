dffc592222a0d76797c2f8b8c7cf8b201f7fde66
Igor Lukanin <me@igorlukanin.io>
2020-07-02 18:08:30 +0300

    Make a HTTP request

--- app/Main.java
+++ app/Main.java
@@ -1,5 +1,24 @@
+import java.net.*;
+import java.net.http.*;
+
 class Main {
-    public static void main(String[] args) {
-        System.out.println(args[0] + " " + args[1]);
+    public static void main(String[] args) throws Exception {
+        var serverUrl = args[0];
+        var playerKey = args[1];
+
+        System.out.println("ServerUrl: " + serverUrl + "; PlayerKey: " + playerKey);
+
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + "?player_key=" + playerKey))
+                .build();
+
+        var response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.discarding());
+
+        var status = response.statusCode();
+
+        if (status != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed to send request. Status code: " + status);
+        }
     }
 }
